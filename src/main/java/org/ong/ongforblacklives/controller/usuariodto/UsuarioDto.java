dcec80eab9559847a475a830a7cb782aecc202a6
Marcio Santos Bernadino <Marcio.Santos1@ibm.com>
2022-08-12 20:17:22 -0300

    DTO implementado!

--- src/main/java/org/ong/ongforblacklives/controller/usuariodto/UsuarioDto.java
+++ src/main/java/org/ong/ongforblacklives/controller/usuariodto/UsuarioDto.java
@@ -1,41 +1,55 @@
 package org.ong.ongforblacklives.controller.usuariodto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import lombok.experimental.Accessors;
+import org.ong.ongforblacklives.model.UsuarioModel;
 
 @Data
+//@Getter
+@Accessors(chain = true)
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class UsuarioDto {
-
-    private Long id;
     private String nome;
     private String cidade;
     private String bairro;
     private String cep;
-    private String cnpj;
     private String email;
     private String area_de_atuacao;
 
-    public UsuarioDto(UsuarioDto usuarioDto) {
-        this.id = usuarioDto.getId();
+    public UsuarioDto build(UsuarioModel usuarioModel){
+        UsuarioDto usuarioDto = new UsuarioDto()
+                .setNome(usuarioModel.nome)
+                .setCidade(usuarioModel.cidade)
+                .setBairro(usuarioModel.getBairro())
+                .setCep(usuarioModel.cep)
+                .setEmail(usuarioModel.email)
+                .setArea_de_atuacao(usuarioModel.area_de_atuacao);
+        return usuarioDto;
+    }
+
+    /*public UsuarioDto(UsuarioDto usuarioDto) {
         this.nome = usuarioDto.getNome();
         this.cidade = usuarioDto.getCidade();
         this.bairro = usuarioDto.getBairro();
         this.cep = usuarioDto.getCep();
-        this.cnpj = usuarioDto.getCnpj();
         this.email = usuarioDto.getEmail();
         this.area_de_atuacao = usuarioDto.getArea_de_atuacao();
     }
 
-    public static List<UsuarioDto> convert(List<UsuarioDto> usuarioDtos){
-        return usuarioDtos.stream().map(UsuarioDto::new).collect(Collectors.toList());
+    public UsuarioDto(UsuarioModel usuarioModel) {
 
     }
-}
+
+    public UsuarioDto(Usuario entity) {
+
+    }
+
+    public static List<UsuarioDto> convert(List<UsuarioModel> usuarioModels){
+        return usuarioModels.stream().map(UsuarioDto::new).collect(Collectors.toList());*/
+
+    }
+
