da8be90a1e1266620bc6f5e89df37bff90b0ac11
Marcio Santos Bernadino <Marcio.Santos1@ibm.com>
2022-11-08 14:11:12 -0300

    Repositori

--- src/main/java/org/ong/ongforblacklives/OngForBlackLivesApplication.java
+++ src/main/java/org/ong/ongforblacklives/OngForBlackLivesApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OngForBlackLivesApplication<UsuarioRepository> implements CommandLineRunner {
 
